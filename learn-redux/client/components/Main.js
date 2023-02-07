2d1daeac8ed5440fe3b1e525c3d097b3d55615f2
Ryan Mansfield <ryan.mansfield@launchpadlab.com>
2023-02-07 12:37:21 -0500

    Chapter 4

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const Main = () => {
+const Main = (props) => {
   return (
     <div>
       <h1>
         <Link to="/">Reduxstagram</Link>
       </h1>
-      {React.cloneElement(this.props.children, this.pros)}
+      {React.cloneElement(props.children, props)}
     </div>
   )
 }
