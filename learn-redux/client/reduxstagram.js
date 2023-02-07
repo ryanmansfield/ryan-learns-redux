57b17e4edfa67f7121071ab9debbda79851f575f
Ryan Mansfield <ryan.mansfield@launchpadlab.com>
2023-02-07 12:16:35 -0500

    Chapters 1 and 2

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,10 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+// import css
+import css from './styles/style.styl'
+
+// components
+import Main from './components/Main'
+
+render(<Main />, document.getElementById('root'));
