69f234635987945c7bdb1bcd57c8b1fa4d08d6be
Ryan Mansfield <ryan.mansfield@launchpadlab.com>
2023-02-07 18:13:59 -0500

    Chapter 17

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -9,6 +9,23 @@ import App from './components/App'
 import Single from './components/Single'
 import PhotoGrid from './components/PhotoGrid'
 
+// Sentry Error Handling
+// import Raven from 'raven';
+// import { sentry_url, logException } from './data/config';
+// Raven.config(sentry_url, {
+//   tags: {
+//     git_commit: '123',
+//     userLevel: 'editor'
+//   }
+// }).install();
+ 
+// logException(new Error('download failed!'), {
+//   email: 'foo@bar.com'
+// });
+
+// Raven.captureMessage('Something Bad Happened!');
+// Raven.showReportDialog();
+
 // import react router deps
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
